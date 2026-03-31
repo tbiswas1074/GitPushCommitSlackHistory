fccb61b4f4ddfea11c1aa6d6ba3868a1cb59ba3e
tbiswas1074 <biswastuhin1074@gmail.com>
2026-03-31 16:46:45 +0530

    message modification

--- src/main/java/com/gituserhistory/admin/controller/GithubWebhookController.java
+++ src/main/java/com/gituserhistory/admin/controller/GithubWebhookController.java
@@ -5,10 +5,7 @@ import com.gituserhistory.admin.dto.GithubPushEvent;
 import com.gituserhistory.admin.service.GithubService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -25,7 +22,12 @@ public class GithubWebhookController {
 
     @PostMapping
     public ResponseEntity<Void> handlePush(
+            @RequestHeader("X-GitHub-Event") String eventType,
             @RequestBody GithubPushEvent event) {
+
+        if (!"push".equals(eventType)) {
+            return ResponseEntity.ok().build();
+        }
 
         githubService.processPushEvent(event);
         return ResponseEntity.ok().build();
